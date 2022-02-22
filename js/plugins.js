63e14228ec92aa8b066f87c8ab78e0c9ee984b78
Med Amine Ck <medamineck4u@gmail.com>
2022-02-22 23:31:32 +0100

    first commit

---
+++ js/plugins.js
@@ -0,0 +1,84 @@
+// Avoid `console` errors in browsers that lack a console.
+(function () {
+  var method;
+  var noop = function () {};
+  var methods = [
+    "assert",
+    "clear",
+    "count",
+    "debug",
+    "dir",
+    "dirxml",
+    "error",
+    "exception",
+    "group",
+    "groupCollapsed",
+    "groupEnd",
+    "info",
+    "log",
+    "markTimeline",
+    "profile",
+    "profileEnd",
+    "table",
+    "time",
+    "timeEnd",
+    "timeline",
+    "timelineEnd",
+    "timeStamp",
+    "trace",
+    "warn",
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+})();
+
+// Place any jQuery/helper plugins in here.
+const swiper_cover = new Swiper(".swiper-cover", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Place any jQuery/helper plugins in here.
+const swiper_gallery = new Swiper(".swiper-gallery", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // Default parameters
+  slidesPerView: 2,
+  spaceBetween: 10,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
